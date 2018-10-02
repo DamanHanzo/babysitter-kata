a1d00593c24cb178955f98aea7435f27bdc281ef
damanahanzo <singhdamanpreet1234@gmail.com>
2018-10-02 11:10:40 -0400

    added assert statements to test both scenarios of the exception

--- babysitter-tests.py
+++ babysitter-tests.py
@@ -31,6 +31,7 @@ class BabySitterTests(unittest.TestCase):
 	def test_verify_entered_time_ranges(self):
 		'''Validate the entered hours for the babysitter'''
 		self.assertRaises(Exception, self.sitter._verifyTimeRanges, 17, 16, 21)
+		self.assertRaises(Exception, self.sitter._verifyTimeRanges, 17, 5, 21)
 
 if __name__ == '__main__':
 	unittest.main()
