7488052f40fc534329c4fd0fbc9204fc8cd63f1b
damanahanzo <singhdamanpreet1234@gmail.com>
2018-10-02 13:46:35 -0400

    refactored the tests to using getter methods instead of the private variables

--- babysitter-tests.py
+++ babysitter-tests.py
@@ -61,9 +61,9 @@ class BabySitterTests(unittest.TestCase):
 		self.assertEqual(backupsitter._calcHoursMidnightTillEnd(), hours)
 
 	def test_wage_rate_constants(self):
-		self.assertEqual(self.sitter._hourlyWageTillBedTime, 12)
-		self.assertEqual(self.sitter._hourlyWageBedTimeTillMidnight, 8)
-		self.assertEqual(self.sitter._hourlyWageMidnightTillEnd, 16)
+		self.assertEqual(self.sitter.hourly_wage_till_bed_time(), 12)
+		self.assertEqual(self.sitter.hourly_wage_bedtime_till_midnight(), 8)
+		self.assertEqual(self.sitter.hourly_wage_midnight_till_end(), 16)
 
 if __name__ == '__main__':
 	unittest.main()
