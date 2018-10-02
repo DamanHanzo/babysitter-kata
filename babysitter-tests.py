7834b12c9bf1a20e6fda2b7066a780cec10fe589
damanahanzo <singhdamanpreet1234@gmail.com>
2018-10-02 07:59:33 -0400

    removed constructor arguments; because those come after

--- babysitter-tests.py
+++ babysitter-tests.py
@@ -5,7 +5,7 @@ class BabySitterTests(unittest.TestCase):
 
 	def setUp(self):	
 		'''Setup the babysitter once so that it can't be utilized all throughout the test'''
-		self.sitter = BabySitter(17, 21, 22)
+		self.sitter = BabySitter()
 		
 	def test_if_babysitter_object_exists(self):
 		'''Test if the babysitter object exists'''
