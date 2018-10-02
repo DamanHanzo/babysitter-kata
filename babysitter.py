0e4dedb91b91c1be9e3af9f1f8103207114bd384
damanahanzo <singhdamanpreet1234@gmail.com>
2018-10-02 11:13:04 -0400

    added _verifyTimeRanges method to make the test_verify_entered_time_ranges test method pass

--- babysitter.py
+++ babysitter.py
@@ -8,6 +8,13 @@ class BabySitter:
 		self._sitterEndTime = 4
 		self._initialBedTime = 21
 
+	def _verifyTimeRanges(self, startTime, endTime, bedTime):
+		'''Check to make sure that the input time meets the sitter's time constraints'''
+		if startTime < self._sitterStartTime and startTime > self._sitterEndTime:
+			raise Exception("I am not available before 5:00 PM, sorry")
+		elif endTime < self._sitterStartTime and endTime > self._sitterEndTime:
+			raise Exception("I cannnot stay after 4:00 AM")
+
 	def sitter_start_time(self):
 		return self._sitterStartTime
 
